e038f873058a999e7e96baad6f55a8671a4b6cf2
YEAHx4 <developerstarlight@gmail.com>
2025-05-08 15:59:32 +0900

    feat: node.js project layout

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
