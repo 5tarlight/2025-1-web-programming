2aec0ebdbc85f8f17ebbcfd89b453687c8bfe466
YEAHx4 <developerstarlight@gmail.com>
2025-05-24 13:56:44 +0900

    feat: add accordion library

--- src/public/js/private/example.js
+++ src/public/js/private/example.js
@@ -5,4 +5,10 @@ afterLoad(() => {
   PopupMenu.attach(trigger, popup, {
     position: "bottom-left",
   });
+
+  document.querySelectorAll(".accordion-item").forEach((item) => {
+    const trigger = item.querySelector(".accordion-trigger");
+    const content = item.querySelector(".accordion-content");
+    Accordion.attach(trigger, content);
+  });
 });
